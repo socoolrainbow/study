43e83c6abf94ee719ec8093fcd1e8d115bb285c7
Hyunsoo <socoolrainbow@gmail.com>
2014-11-12 10:41:14 +0900

    delete

--- Study/study/JavaSkillBook/chapter6/AbstractHandler.java
+++
@@ -1,6 +0,0 @@
-package chapter6;
-
-public abstract class AbstractHandler
-{
-    public abstract String getRangeInfo();
-}
--- Study/study/JavaSkillBook/chapter6/GeneratorInterface.java
+++
@@ -1,9 +0,0 @@
-package chapter6;
-
-public interface GeneratorInterface
-{
-    public static final String ID_HEADER = "UNIQUE";
-    public static final String DELIMITER = "_";
-    public abstract String getUniqueValue();
-    
-}
